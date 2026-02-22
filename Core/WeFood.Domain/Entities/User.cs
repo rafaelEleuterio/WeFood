3a377e3f0d0f49a8224f5eddf8934939cc06a8bf
rafaelEleuterio <rafale.eleuteiro.sp@gmail.com>
2026-02-21 22:28:57 -0300

    Add: In Domain, Entitties.User and Enums.UserTypes were created.

---
+++ Core/WeFood.Domain/Entities/User.cs
@@ -0,0 +1,21 @@
+﻿using WeFood.Domain.Enums;
+
+namespace WeFood.Domain.Entities;
+
+public class User
+{
+    public Guid Id { get; private set; }
+    public string FullName { get; private set; }
+    public UserType UserType { get; private set; }
+    public bool IsActive { get; private set; }
+
+    protected User() { }
+
+    public User(Guid id, string fullName, UserType userType)
+    {
+        Id = id;
+        FullName = fullName;
+        UserType = userType;
+        IsActive = true;
+    }
+}
---
+++ Core/WeFood.Domain/Enums/UserType.cs
@@ -0,0 +1,9 @@
+﻿namespace WeFood.Domain.Enums;
+
+public enum UserType
+{
+    Customer = 1,
+    Store = 2,
+    Delivery = 3,
+    Admin = 4
+}
