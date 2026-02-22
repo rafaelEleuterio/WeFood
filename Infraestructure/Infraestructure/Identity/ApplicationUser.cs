8444ce81cf85a8310b1af827295da3ceb65ce0cd
rafaelEleuterio <rafale.eleuteiro.sp@gmail.com>
2026-02-22 17:25:30 -0300

    Add: ApplicationUser and RefreshToken

---
+++ Infraestructure/Infraestructure/Identity/ApplicationUser.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Identity;
+using WeFood.Domain.Enums;
+
+namespace Infraestructure.Identity;
+public class ApplicationUser : IdentityUser<Guid>
+{
+    public string FullName { get; set; }
+    public UserType UserType { get; set; }
+}
